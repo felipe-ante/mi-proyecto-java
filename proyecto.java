f5081fabd96c4246eb573066585b9379e68b5d4d
felipe-ante <felpe.ante@gmail.com>
2026-01-28 17:10:17 -0500

    Update proyecto.java
    se crea un programa que le da la bienvenida a cesar

--- proyecto.java
+++ proyecto.java
@@ -1,1 +1,8 @@
-cordial saludo para todos y todas 
+public class proyecto{
+    public static void main(String[] args) 
+    {
+        //este es un programa le da la bienvenida  a cesar  //
+        System.out.println("CESAR, Bienvenido al curso de programacion
+        en Java!!!");
+    }
+}
